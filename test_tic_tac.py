d320cdc660ca8956ba15c08c4b305f810c113b27
RetardedRedhaired <48159420+RetardedRedhaired@users.noreply.github.com>
2020-10-12 20:58:54 +0300

    Add files via upload

---
+++ test_tic_tac.py
@@ -0,0 +1,35 @@
+import unittest
+import random
+from string import printable
+import tic_tac
+
+
+class TicTacTest(unittest.TestCase):
+	def test_check_winner(self):
+		game = []
+		for _ in range(9):
+			game.append(None)
+		for i in range(3):
+			game[i*3] = game[i*3+1] = game[i*3+2] = True
+			self.assertFalse(tic_tac.TicTacGame()._check_winner(game))
+			game[i*3] = game[i*3+1] = game[i*3+2] = None
+			game[i] = game[i+3] = game[i+6] = True
+			self.assertFalse(tic_tac.TicTacGame()._check_winner(game))
+			game[i] = game[i+3] = game[i+6] = None
+		game[0] = game[4] = game[8] = True
+		self.assertFalse(tic_tac.TicTacGame()._check_winner(game))
+		game[0] = game[4] = game[8] = None
+		game[2] = game[4] = game[6] = True
+		self.assertFalse(tic_tac.TicTacGame()._check_winner(game))
+	
+	def test_validate_input(self):
+		tmp = ''
+		print(type(tmp))
+		for _ in range(random.randint(1, 10)):
+			tmp = tmp + random.choice(string.printable)
+		assertRaises(ValueError, tic_tac.TicTacGame()._validate_input(tmp))
+		assertTrue(tic_tac.TicTacGame()._validate_input(random.randint(1, 100))
+
+
+if __name__ == '__main__':
+	unittest.main()
